f605059ca1a666814077135938435cf0df2b3311
sameerji43219 <mdsameer703327@gmail.com>
2025-05-27 11:16:08 +0530

    Update utils.py

--- utils.py
+++ utils.py
@@ -89,12 +89,12 @@ async def progress_bar(current, total, reply, start):
             remaining_length = bar_length - completed_length
 
             symbol_pairs = [
-                ("▬", "▭"),
-                ("✅", "☑️"),
-                ("🐬", "🦈"),
-                ("💚", "💛"),
-                ("🌟", "⭐"),
-                ("▰", "▱")
+                ("●", "○"),
+                ("█", "▒"),
+                ("▮", "▯"),
+                ("◈", "◆"),
+                ("★", "○"),
+                ("■", "□")
             ]
             chosen_pair = random.choice(symbol_pairs)
             completed_symbol, remaining_symbol = chosen_pair
@@ -102,7 +102,7 @@ async def progress_bar(current, total, reply, start):
             progress_bar = completed_symbol * completed_length + remaining_symbol * remaining_length
             
             try:
-                await reply.edit(f'`╭──⌯═════𝐔𝐩𝐥𝐨𝐚𝐝𝐢𝐧𝐠══════⌯──╮\n├⚡ {progress_bar}\n├⚙️ Progress ➤ | {perc} |\n├🚀 Speed ➤ | {sp} |\n├📟 Processed ➤ | {cur} |\n├🧲 Size ➤ | {tot} |\n├🕑 ETA ➤ | {eta} |\n╰─═══✨🦋𝗦𝗔𝗠𝗘𝗘𝗥 𝗝𝗜🦋✨═══─╯`') 
-                #await reply.edit(f'`╭──⌯═════𝐁𝐨𝐭 𝐒𝐭𝐚𝐭𝐢𝐜𝐬══════⌯──╮\n├⚡ {progress_bar}\n├⚙️ Progress ➤ | {perc} |\n├🚀 Speed ➤ | {sp} |\n├📟 Processed ➤ | {cur} |\n├🧲 Size ➤ | {tot} |\n├🕑 ETA ➤ | {eta} |\n╰─═══✨🦋𝗦𝗔𝗠𝗘𝗘𝗥 𝗝𝗜🦋✨═══─╯`') 
+                await reply.edit(f'`╭── ⋅ ⋅ ── ​🇺​​🇵​​🇱​​🇴​​🇦​​🇩​​🇮​​🇳​​🇬​✩ ── ⋅ ⋅ ──╮\n├⚡ {progress_bar}\n├⚙️ Progress ➤ | {perc} |\n├🚀 Speed ➤ | {sp} |\n├📟 Processed ➤ | {cur} |\n├🧲 Size ➤ | {tot} |\n├🕑 ETA ➤ | {eta} |\n╰── ⋅ ⋅ ── 🇮🇳 🦂𝗚𝗨𝗥𝗨 𝗝𝗜🦂 🇮🇳✩ ── ⋅ ⋅ ──╯`') 
+                #await reply.edit(f'`╭──⌯═════𝐁𝐨𝐭 𝐒𝐭𝐚𝐭𝐢𝐜𝐬══════⌯──╮\n├⚡ {progress_bar}\n├⚙️ Progress ➤ | {perc} |\n├🚀 Speed ➤ | {sp} |\n├📟 Processed ➤ | {cur} |\n├🧲 Size ➤ | {tot} |\n├🕑 ETA ➤ | {eta} |\n╰── ⋅ ⋅ ── 🇮🇳 🦂𝗚𝗨𝗥𝗨 𝗝𝗜🦂 🇮🇳✩ ── ⋅ ⋅ ──╯`') 
             except FloodWait as e:
                 time.sleep(e.x)
